875fbf678eeb3dd77f34bc20772a6bc2723a0c38
YuJian <zhonghao@jobcn.com>
2022-04-27 15:42:56 +0800

    Web 样式调整

--- YuJianBlog-Web/src/components/Footer/index.tsx
+++ YuJianBlog-Web/src/components/Footer/index.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./index.less";
 
 const Footer = () => {
-  return <footer className="footer">Powered By React</footer>;
+  return (
+    <footer className="footer">
+      <span>Copyright © 2016-2022 YuJian All Rights Reserved</span>
+      <a className="footer__bn" href="https://beian.miit.gov.cn">
+        粤ICP备2022028534号
+      </a>
+    </footer>
+  );
 };
 
 export default React.memo(Footer);
--- YuJianBlog-Web/src/pages/Layout/index.tsx
+++ YuJianBlog-Web/src/pages/Layout/index.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../../components/Navigation";
+import Footer from "../../components/Footer";
+import "./index.less";
 
 const Layout = () => {
   return (
-    <div className="App">
+    <div className="layout">
       <Navigation />
-      <Outlet />
+      <div className="article">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
