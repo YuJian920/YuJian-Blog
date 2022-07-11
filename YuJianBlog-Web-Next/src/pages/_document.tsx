580a767f3364581b6b8782897ff2653cb0163b04
YuJian920 <HAO52013141@Gmail.com>
2022-07-11 19:54:45 +0800

    Web-Next 添加 document

---
+++ YuJianBlog-Web-Next/src/pages/_document.tsx
@@ -0,0 +1,11 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default () => (
+  <Html lang="zh-cmn-Hans">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
