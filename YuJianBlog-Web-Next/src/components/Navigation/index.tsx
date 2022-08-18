1d074064786a150292ed1b3e38bedb47ee86f0f6
YuJian <zhonghao@jobcn.com>
2022-08-18 15:33:31 +0800

    Web-Next 细节调整

--- YuJianBlog-Web-Next/src/components/Navigation/index.tsx
+++ YuJianBlog-Web-Next/src/components/Navigation/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { SVGProps, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import YuJianLogo from "../../assets/images/YuJianLogo.webp";
@@ -9,7 +9,56 @@ const linkMap = [
   { name: "关于", path: "/About" },
 ];
 
-const Navigation = () => {
+const Navigation = () => (
+  <div className={style.pageHeader}>
+    <div className={style.pageHeader__container}>
+      <span className={style["pageHeader__container-left"]}>
+        <div className={style.leftImg}>
+          <Image src={YuJianLogo} alt="YuJianLogo" layout="responsive" />
+        </div>
+        <span className={style.leftSpan}>
+          <Link href="/">YuJian's Blog</Link>
+        </span>
+      </span>
+      <nav className={style.pageHeader__nav}>
+        {linkMap.map((linkItem) => (
+          <Link href={linkItem.path} key={linkItem.name}>
+            {linkItem.name}
+          </Link>
+        ))}
+        <DarkModeButton />
+      </nav>
+    </div>
+  </div>
+);
+
+/**
+ * svg 图标的配置字典
+ */
+const svgConfigDict: SVGProps<SVGSVGElement>[] = [
+  {
+    className: style.svg,
+    viewBox: "0 0 24 24",
+    fill: "none",
+    stroke: "currentColor",
+    strokeWidth: "2",
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+  },
+  {
+    className: style.svg,
+    width: "24",
+    height: "24",
+    viewBox: "0 0 24 24",
+    strokeWidth: "2",
+    stroke: "currentColor",
+    fill: "none",
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+  },
+];
+
+const DarkModeButton = () => {
   const [darkMode, setDarkMode] = useState(false); // 深色模式状态
 
   // 切换 深色模式
@@ -29,58 +78,19 @@ const Navigation = () => {
     setDarkMode(value);
   };
 
-  return (
-    <div className={style.pageHeader}>
-      <div className={style.pageHeader__container}>
-        <span className={style["pageHeader__container-left"]}>
-          <div className={style.leftImg}>
-            <Image src={YuJianLogo} alt="YuJianLogo" layout="responsive" />
-          </div>
-          <span className={style.leftSpan}>
-            <Link href="/">YuJian's Blog</Link>
-          </span>
-        </span>
-        <nav className={style.pageHeader__nav}>
-          {linkMap.map((linkItem) => (
-            <Link href={linkItem.path} key={linkItem.name}>
-              {linkItem.name}
-            </Link>
-          ))}
-          {!darkMode ? (
-            <svg
-              className={style.svg}
-              onClick={() => onDarkMode(true)}
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
-            </svg>
-          ) : (
-            <svg
-              className={style.svg}
-              onClick={() => onDarkMode(false)}
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" />
-              <circle cx="12" cy="12" r="4" />
-              <path d="M3 12h1M12 3v1M20 12h1M12 20v1M5.6 5.6l.7 .7M18.4 5.6l-.7 .7M17.7 17.7l.7 .7M6.3 17.7l-.7 .7" />
-            </svg>
-          )}
-        </nav>
-      </div>
-    </div>
+  const svgConfig = darkMode ? svgConfigDict[0] : svgConfigDict[1];
+
+  return !darkMode ? (
+    <svg onClick={() => onDarkMode(true)} {...svgConfig}>
+      <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+    </svg>
+  ) : (
+    <svg onClick={() => onDarkMode(false)} {...svgConfig}>
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <circle cx="12" cy="12" r="4" />
+      <path d="M3 12h1M12 3v1M20 12h1M12 20v1M5.6 5.6l.7 .7M18.4 5.6l-.7 .7M17.7 17.7l.7 .7M6.3 17.7l-.7 .7" />
+    </svg>
   );
 };
 
-export default React.memo(Navigation);
+export default Navigation;
